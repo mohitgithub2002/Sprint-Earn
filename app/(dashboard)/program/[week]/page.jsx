c6249220a02cda7d2763ad9310c283d5dacbf598
Mohit Goyal <mohit.work2002@gmail.com>
2024-08-08 23:09:45 +0530

    Update: make the program page client component

--- app/(dashboard)/program/[week]/page.jsx
+++ app/(dashboard)/program/[week]/page.jsx
@@ -1,48 +1,29 @@
+'use client'
 import WeekHeader from "@/components/program/weekheader";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import getData from "../getdata";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 const current = 2
-const days = [
-  {
-    day: "day 1",
-    title: "Discover & Define",
-    isCompleted: true
-  },
-  {
-    day: "day 2",
-    title: "Prototype and Pre-Sell",
-    isCompleted: true
-  },
-  {
-    day: "day 3",
-    title: "Launch & Learn",
-    isCompleted: false
-  },
-  {
-    day: "day 4",
-    title: "Build Your Brand Online",
-    isCompleted: false
-  },
-  {
-    day: "day 5",
-    title: "Iterate and Improve",
-    isCompleted: false
-  },
-  {
-    day: "day 6",
-    title: "Sales & Marketing Mastery",
-    isCompleted: false
-  }
-]
 
-const Program = async({params}) => {
-    const data = await getData();
+
+const Program = ({params}) => {
     const week = params.week
     const weekNumber = week.match(/\d+/)[0];
     console.log("my week is =>"+weekNumber)
-    const weekDetail = data[weekNumber-1]
+    const [weekDetail, setWeekDetail] = useState();
+    useEffect(()=>{
+      const fetchData = async()=>{
+        const data = await getData();
+        setWeekDetail(data[weekNumber-1])
+        
+      }
+      fetchData();
+    },[])
+    
+    
     console.log(weekDetail)
+    if(!weekDetail) return <div>Loading...</div>
     return (
       <div className="h-screen bg-white flex flex-col flex-1 space-y-4 px-8 overflow-scroll">
         <div className="pt-4">
@@ -52,7 +33,7 @@ const Program = async({params}) => {
           <div className=" w-2/3 text-black p-4 space-y-4">
               <h1 className="text-xl font-bold">Task List</h1>
               <div className="flex flex-col space-y-6">
-                {weekDetail.days.map((day, index) => (
+                {weekDetail&&weekDetail.days.map((day, index) => (
                    <div key={index}>
                       <div className="flex pl-8 pr-3 py-2 justify-between items-center border border-slate-700 rounded-full shadow-md hover:scale-[1.02] transition-all"> 
                         <div><i className={`far fa-calendar mr-3`}></i>{`Day ${index+1}`}</div>
--- app/(dashboard)/program/page.jsx
+++ app/(dashboard)/program/page.jsx
@@ -1,12 +1,20 @@
-
+'use client'
 import CourseCard from "@/components/program/weekcard";
 import WelcomeCard from "@/components/program/welcome";
 import getData from "./getdata";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 
-const Program = async() => {
-  const data = await getData();
+const Program = () => {
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    const fetchData = async()=>{
+      const data = await getData();
+      setData(data);
+    }
+    fetchData();
+  },[])
   console.log(data)
     return (
       <div className="h-screen bg-white flex flex-col flex-1 space-y-4 px-8 overflow-scroll">
@@ -16,7 +24,7 @@ const Program = async() => {
         <div className="space-y-4">
           <h1 className=" font-semibold text-2xl text-black">Program Details</h1>
           <div className="grid grid-flow-row grid-cols-3 gap-4">
-            {data.map((data,index)=>{
+            {data&&data.map((data,index)=>{
               return <Link href={`/program/week${index+1}`} key={index}><CourseCard data={data} /></Link>
           })}
           </div>
