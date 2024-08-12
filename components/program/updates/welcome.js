ac51a13aab6809abe8bc84bc0353e2e800684b9b
Mohit Goyal <mohit.work2002@gmail.com>
2024-08-12 15:59:39 +0530

    feat: Update welcome message and button text

--- components/program/updates/welcome.js
+++ components/program/updates/welcome.js
@@ -13,11 +13,11 @@ const Welcome = ({setIsForm,goBack,day,week}) => {
           time to submit<br /> update #{day}
         </h1>
         <p className="text-gray-400 mb-8">
-          If you're working with a team, be sure each member on your team submits their own updates.
+          Describe the progress of your idea attach related imagesor video for review
         </p>
         <div className="flex justify-center space-x-4">
           <button onClick={()=>setIsForm(true)} className="bg-cta text-white py-2 px-4 rounded-md hover:scale-105">
-          i'm ready
+          i&apos;m ready
           </button>
           <button onClick={()=>goBack()} className="bg-white text-black py-2 px-4 rounded-md hover:bg-gray-200">
             go-back
