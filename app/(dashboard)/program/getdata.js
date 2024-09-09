4df5a7013d4c00aaebb3c5dd6f0324464bd69614
Mohit Goyal <mohit.work2002@gmail.com>
2024-09-09 21:54:17 +0530

    Update: getData.js file required in [days details page

--- app/(dashboard)/program/getdata.js
+++ app/(dashboard)/program/getdata.js
@@ -1,6 +1,6 @@
 export default async function getData() {
   
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/weeklist`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/weeklist`, { cache: 'no-store' })
     const projects = await res.json()
     const data = projects.data;
     return data
