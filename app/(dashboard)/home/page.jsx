944c0ae9da141605a0a23bf3737a0abaf9d9b99e
Mohit Goyal <mohit.work2002@gmail.com>
2024-09-09 22:07:57 +0530

    feat: Update home page to display user's details dynamically

--- app/(dashboard)/home/page.jsx
+++ app/(dashboard)/home/page.jsx
@@ -1,25 +1,25 @@
-"use client";
-
 import ChatBox from "@/components/home/chatbox";
 import ProgressCards from "@/components/home/progress";
 import PremiumCard from "@/components/home/task";
+import { UserDetail } from "@/utils/userdetail";
 
-const Home = () => {
+const Home = async() => {
+  const data = await UserDetail()
   return (
     <div className="min-h-screen bg-white flex flex-col space-y-4 px-4 md:px-8 text-black pb-20 md:pb-0">
       <div className="text-lg md:text-xl font-bold mt-4 text-left">
-        👋 Welcome, Mohit Goyal!
+        👋 Welcome, {data.name}!
       </div>
       <div className="w-full">
         <ChatBox />
       </div>
       <div className="w-full space-y-4">
         <h1 className="font-semibold text-xl md:text-2xl text-bg-primary">Progress</h1>
-        <ProgressCards />
+        <ProgressCards data={data} />
       </div>
       <div className="space-y-4">
         <h1 className="font-semibold text-xl md:text-2xl text-bg-primary">Update</h1>
-        <PremiumCard />
+        <PremiumCard data ={data} />
       </div>
     </div>
   );
